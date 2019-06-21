8d7fc9b1bdce943bafc7a1bda0cb28f08c865c14
Wilton Garcia <wiltongarciaweb@gmail.com>
2019-06-20 22:52:58 -0300

    Commit 0002
    Projeto console HelloWorld

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Distribuição do Linux: Ubuntu 18.04");
+            Console.WriteLine("Ferramenta para edição de código: Visual Studido Code");
+        }
+    }
+}
